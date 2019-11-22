b6143c6947af5e0f31865adb465a79ee53babd38
bruno <bfpalacios@gmail.com>
2019-11-22 08:09:38 -0500

    Codigo de metodos separados por capas y la funcionalidad unitaria terminada.

---
+++ integracion_scraping/src/main/java/com/belatrixsf/integracion_scraping/modelo/ScrapingWeb.java
@@ -0,0 +1,21 @@
+package com.belatrixsf.integracion_scraping.modelo;
+
+public class ScrapingWeb {
+	
+	private String tag;
+	private String tipo;
+	public String getTag() {
+		return tag;
+	}
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	} 
+	
+	
+}
--- integracion_scraping/src/main/java/com/belatrixsf/integracion_scraping/scrapingweb/MyScrapingWeb.java
+++ integracion_scraping/src/main/java/com/belatrixsf/integracion_scraping/scrapingweb/MyScrapingWeb.java
@@ -9,95 +9,158 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class MyScrapingWeb {
-	
-	
-public static final String url = "http://www.yahoo.com";
-	
-    public static void main (String args[]) {
-		
-    	System.out.println("Inicio");
-		
-        // Compruebo si me da un 200 al hacer la petición
-        if (getStatusConnectionCode(url) == 200) {
 
-        	System.out.println("entro");
-            // Obtengo el HTML de la web en un objeto Document
-            Document document = getHtmlDocument(url);
-			
-            
-            Element body = document.body();
-            
-            
-            // Busco todas las entradas que estan dentro de: 
-            Elements entradas = body.getAllElements();
-            
-            System.out.println("Número de entradas en la página inicial de Jarroba: "+entradas.size()+"\n");
-			
-            
-            StringBuilder tmp = new StringBuilder();
-            // Paseo cada una de las entradas
-            for (Element elem : entradas) {
-                //String hashTag = elem.getElementsByClass("#").text();
-                //String twitter = elem.getElementsByClass("@").toString();
-                String fecha = elem.getElementsByClass("fecha").toString();
+	// atributos de instancia
+	// private StringBuilder contenidoArchivoOutput;
 
-                //System.out.println(hashTag+"\n"+twitter+"\n"+fecha+"\n\n");
-                System.out.println("Fecha "+fecha+"\n\n");
-				
-                // Con el método "text()" obtengo el contenido que hay dentro de las etiquetas HTML
-                // Con el método "toString()" obtengo todo el HTML con etiquetas incluidas
-                
+	public static final String url = "https://guiasbus.us.es/citarredessociales/twitter"; 
 
-                //tmp.append(hashTag).append("\n");
-                //tmp.append(twitter).append("\n");
-                tmp.append(fecha).append("\n");
-            }
-				
-        }else
-            System.out.println("El Status Code no es OK es: "+getStatusConnectionCode(url));
-    }
- 
+	public static void main(String args[]) {
+		procesarScrapingWeb("", "", url);
+	}
+	
 	
 	/**
-	 * Con esta método compruebo el Status code de la respuesta que recibo al hacer la petición
-	 * EJM:
-	 * 		200 OK			300 Multiple Choices
-	 * 		301 Moved Permanently	305 Use Proxy
-	 * 		400 Bad Request		403 Forbidden
-	 * 		404 Not Found		500 Internal Server Error
-	 * 		502 Bad Gateway		503 Service Unavailable
+	 * Con esta método se procesan los tags a buscar por tipo 
+	 * en las urls solicitadas. Al finalizar las busquedas 
+	 * guarda en un archivo de texto de salida toda la informacion
+	 * recopilada.
+	 * 
+	 * @param tagABuscar
+	 * @param tipoTag
+	 * @param url 
+	 */
+	public static void procesarScrapingWeb(String tagABuscar, String tipoTag, String url) {
+		// Compruebo si me da un 200 al hacer la petición
+		if (getStatusConnectionCode(url) == 200) {
+
+			// Obtengo el HTML de la web en un objeto Document
+			Document document = getHtmlDocument(url);
+
+			buscarTagPorTipo(tagABuscar, tipoTag, document);
+
+		} else
+			System.out.println("El Status Code no es OK es: " + getStatusConnectionCode(url));
+	}
+
+	public static void buscarTagPorTipo(String tagABuscar, String tipo, Document document) {
+
+		
+		// Busco todas las entradas que estan dentro de:
+		Element body = document.body();
+		Elements entradas = body.getAllElements();
+
+		Elements hashTags = entradas.select("a[href]"); // Select all links
+
+		
+		//objeto para guardar las entradas de las paginas webs
+		StringBuilder contenidoArchivoSalida = new StringBuilder();
+
+		// referencias a los hashtags con #
+		for (Element elem : hashTags) {
+			String hashTagConMichiHTML = elem.attr("a[href*=#]").toString();
+			String hashTagConMichi = elem.select("a[href*=#]").text();
+
+			if (!hashTagConMichiHTML.equals("")) {
+				System.out.println("hashTagConMichiHTML " + hashTagConMichiHTML + "\n");
+				System.out.println("hashTagConMichi " + hashTagConMichi + "\n");
+			}
+			// Con el método "text()" obtengo el contenido que hay dentro de las etiquetas
+			// HTML
+			// Con el método "toString()" obtengo todo el HTML con etiquetas incluidas
+
+			contenidoArchivoSalida.append(hashTagConMichi).append("\n");
+		}
+
+		for (Element elem : hashTags) {
+			String hrefHTML = elem.select("a[href]").toString();
+			String href = elem.select("a[href]").text();
+			if (!href.equals("")) {
+				System.out.println("href " + href + "\n");
+				System.out.println("hrefHTML " + hrefHTML + "\n");
+			}
+			// Con el método "text()" obtengo el contenido que hay dentro de las etiquetas
+			// HTML
+			// Con el método "toString()" obtengo todo el HTML con etiquetas incluidas
+
+			if (!href.equals("")) {
+				contenidoArchivoSalida.append(href).append("\n");
+			}
+
+		}
+
+		// Twitter markup documentation:
+		// https://dev.twitter.com/cards/markup
+		String[] twitterTags = { "twitter:site", "twitter:site:id", "twitter:creator", "twitter:creator:id",
+				"twitter:description", "twitter:title", "twitter:image", "twitter:image:alt", "twitter:player",
+				"twitter:player:width", "twitter:player:height", "twitter:player:stream", "twitter:app:name:iphone",
+				"twitter:app:id:iphone", "twitter:app:url:iphone", "twitter:app:name:ipad", "twitter:app:id:ipad",
+				"twitter:app:url:ipadt", "twitter:app:name:googleplay", "twitter:app:id:googleplay",
+				"twitter:app:url:googleplay" };
+
+		// Connect to URL and extract source code
+		Document doc;
+		try {
+			doc = Jsoup.connect("http://stackoverflow.com/").get();
+
+			for (String twitterTag : twitterTags) {
+
+				// find a matching meta tag
+				Element meta = doc.select("meta[name=" + twitterTag + "]").first();
+
+				// if found, get the value of the content attribute
+				String content = meta != null ? meta.attr("content") : "";
+
+				// display results
+				System.out.printf("%s = %s%n", twitterTag, content);
+			}
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * Con esta método compruebo el Status code de la respuesta que recibo al hacer
+	 * la petición EJM: 200 OK 300 Multiple Choices 301 Moved Permanently 305 Use
+	 * Proxy 400 Bad Request 403 Forbidden 404 Not Found 500 Internal Server Error
+	 * 502 Bad Gateway 503 Service Unavailable
+	 * 
 	 * @param url
 	 * @return Status Code
 	 */
 	public static int getStatusConnectionCode(String url) {
 		System.out.println("getStatusConnectionCode");
-	    	
-	    Response response = null;
-		
-	    try {
-		response = Jsoup.connect(url).userAgent("Mozilla/5.0").timeout(1000000).ignoreHttpErrors(true).execute();
-	    } catch (IOException ex) {
-		System.out.println("Excepción al obtener el Status Code: " + ex.getMessage());
-	    }
-	    return response.statusCode();
+
+		Response response = null;
+
+		try {
+			response = Jsoup.connect(url).userAgent("Mozilla/5.0").timeout(1000000).ignoreHttpErrors(true).execute();
+		} catch (IOException ex) {
+			System.out.println("Excepción al obtener el Status Code: " + ex.getMessage());
+		}
+		return response.statusCode();
 	}
-	
-	
+
 	/**
 	 * Con este método devuelvo un objeto de la clase Document con el contenido del
-	 * HTML de la web que me permitirá parsearlo con los métodos de la librelia JSoup
+	 * HTML de la web que me permitirá parsearlo con los métodos de la librelia
+	 * JSoup
+	 * 
 	 * @param url
 	 * @return Documento con el HTML
 	 */
 	public static Document getHtmlDocument(String url) {
 
-	    Document doc = null;
+		Document doc = null;
 		try {
-		    doc = Jsoup.connect(url).userAgent("Mozilla/5.0").timeout(100000).get();
-		    } catch (IOException ex) {
+			doc = Jsoup.connect(url).userAgent("Mozilla/5.0").timeout(100000).get();
+		} catch (IOException ex) {
 			System.out.println("Excepción al obtener el HTML de la página" + ex.getMessage());
-		    }
-	    return doc;
+		}
+		return doc;
 	}
 
 }
